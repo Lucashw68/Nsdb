e4e321b4afc1e4a4427f8ea27bf66e0b4fa114a9
Lucas <me@lucasheitzmann.fr>
2025-09-11 22:53:22 +0200

    update

--- cli/index.js
+++ cli/index.js
@@ -8,10 +8,12 @@ const commands = {
 	'generate:types': path.resolve(__dirname, '../scripts/generate-types.js'),
 	'generate:models': path.resolve(__dirname, '../scripts/generate-models.js'),
 	'generate:stores': path.resolve(__dirname, '../scripts/generate-stores.js'),
+	'generate:entities': path.resolve(__dirname, '../scripts/generate-etities.js'),
 	'generate:all': [
 		path.resolve(__dirname, '../scripts/generate-types.js'),
 		path.resolve(__dirname, '../scripts/generate-models.js'),
 		path.resolve(__dirname, '../scripts/generate-stores.js'),
+		path.resolve(__dirname, '../scripts/generate-entities.js'),
 	]
 		.map(p => `node "${p}"`)
 		.join(' && ')
