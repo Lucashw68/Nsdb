99cebe5ac8e3ff86b664a6ed06d7c0ff1c830387
Lucas Heitzmann <me@lucasheitzmann.fr>
2025-10-30 22:14:27 +0100

    update

--- runtime/composables/useSupabaseModels.ts
+++ runtime/composables/useSupabaseModels.ts
@@ -1,5 +1,5 @@
-import { modelMap } from '~/nsdb/models'
-import type { ModelTypes } from '~/nsdb/models'
+import { modelMap } from '#nsdb/models'
+import type { ModelTypes } from '#nsdb/models'
 import { useSupabaseApi } from './useSupabaseApi'
 import { Ref, ref } from 'vue'
 
